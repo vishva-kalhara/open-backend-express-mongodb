f75f111cd868b4297df7a55f2ddb71abc9b51a26
vishva-kalhara <vishvakalhara@gmail.com>
2024-04-15 09:43:04 +0530

    Server and Express App

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
+const app = express();
+
+// Adding HTTP Headers
+app.use(helmet());
+
+// Development Logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// Rate Limiting
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try agin in an hour!',
+});
+app.use('/api', limiter);
+
+// Body Parser
+app.use(express.json({ max: '10kb' }));
+
+// Data sanitization against NoSql Injection
+app.use(mongoSanitize());
+
+// Data sanitization against Cross Side Scripts
+app.use(xss());
+
+// Preventing parameter polution
+app.use(
+  hpp({
+    // whitelist: ['']
+  }),
+);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// 404 Error handling route
+// app.all('*', app)
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('listening...');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = require('./app');
+
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception! Server is shutting down...');
+  console.log(err.name, err.message);
+  // console.log(err)
+
+  process.exit(1);
+});
+
+dotenv.config({ path: './config101.env' });
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection! Server is shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
